ac7f586a1a3bdb5e29035baa68728f6b3c88848f
QinYi896 <1751479263@qq.com>
2022-09-07 19:29:04 +0800

    2.水的模拟和浮力的实现

---
+++ Assets/QinYILearnProject/2.水的模拟和浮力的实现/Water/Scripts/Buoyancy/BuoyancyObject.cs
@@ -0,0 +1,193 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace BuoyancyScope
+{
+    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(MeshFilter))]
+    public class BuoyancyObject : MonoBehaviour
+    {
+        public bool debugForce;
+        public bool debugVoxels;
+
+        [SerializeField]
+        private bool calculateDensity;
+
+        [SerializeField]
+        private float density = 0.75f;
+
+        [SerializeField]
+        [Range(0f, 1f)]
+        private float normalizedVoxelSize = 0.5f;
+
+        [SerializeField]
+        private float dragInWater = 1f;
+
+        [SerializeField]
+        private float angularDragInWater = 1f;
+
+        private static WaterWaves _water;
+        private List<Collider> _colliders;
+        private Rigidbody _rigidbody;
+        private float _initDrag;
+        private float _initAngularDrag;
+        private Vector3 _voxelSize;
+        private Vector3[] _voxels;
+
+
+        public bool isOnTriggerEnterWater = false;
+        protected void Awake()
+        {
+            if (!BuoyancyObject._water) BuoyancyObject._water = FindObjectOfType<WaterWaves>();
+            _colliders = new List<Collider>(GetComponents<Collider>());
+            _rigidbody = GetComponent<Rigidbody>();
+            _initDrag = _rigidbody.drag;
+            _initAngularDrag = _rigidbody.angularDrag;
+
+            if (calculateDensity)
+            {
+                density = _rigidbody.mass / MathfUtils.CalculateVolume_Mesh(GetComponent<MeshFilter>().mesh, transform);
+            }
+        }
+
+        protected void Start()
+        {
+            _voxels = Voxelization();
+        }
+
+        protected void FixedUpdate()
+        {
+            Solver();
+        }
+
+        protected void Solver()
+        {
+            if (BuoyancyObject._water != null && _voxels.Length > 0)
+            {
+                Vector3 forceAtSingleVoxel = CalculateMaxBuoyancyForce() / _voxels.Length;
+                Bounds bounds = _colliders[0].bounds;
+                float voxelHeight = bounds.size.y * normalizedVoxelSize;
+
+                float submergedVolume = 0f;
+                for (int i = 0; i < _voxels.Length; i++)
+                {
+                    Vector3 worldPoint = transform.TransformPoint(_voxels[i]);
+
+                    float waterLevel = BuoyancyObject._water.GetWaterLevel(worldPoint);
+                    float deepLevel = waterLevel - worldPoint.y + (voxelHeight / 2f); // How deep is the voxel                    
+                    float submergedFactor = Mathf.Clamp(deepLevel / voxelHeight, 0f, 1f); // 0 - voxel is fully out of the water, 1 - voxel is fully submerged
+                    submergedVolume += submergedFactor;
+
+                    Vector3 surfaceNormal = BuoyancyObject._water.GetSurfaceNormal(worldPoint);
+                    Quaternion surfaceRotation = Quaternion.FromToRotation(BuoyancyObject._water.transform.up, surfaceNormal);
+                    surfaceRotation = Quaternion.Slerp(surfaceRotation, Quaternion.identity, submergedFactor);
+
+                    Vector3 finalVoxelForce = surfaceRotation * (forceAtSingleVoxel * submergedFactor);
+                    _rigidbody.AddForceAtPosition(finalVoxelForce, worldPoint);
+                 //   Debug.Log(worldPoint + ":worldPoint");
+                    if (debugForce) Debug.DrawLine(worldPoint, worldPoint + finalVoxelForce.normalized, Color.blue);
+                }
+
+                submergedVolume /= _voxels.Length; // 0 - object is fully out of the water, 1 - object is fully submerged
+
+                _rigidbody.drag = Mathf.Lerp(_initDrag, dragInWater, submergedVolume);
+                _rigidbody.angularDrag = Mathf.Lerp(_initAngularDrag, angularDragInWater, submergedVolume);
+            }
+        }
+
+        protected void OnDrawGizmos()
+        {
+            if (debugVoxels && _voxels != null)
+            {
+                for (int i = 0; i < _voxels.Length; i++)
+                {
+                    Gizmos.color = Color.magenta - new Color(0f, 0f, 0f, 0.75f);
+                    Gizmos.DrawCube(transform.TransformPoint(_voxels[i]), _voxelSize * 0.8f);
+                }
+            }
+        }
+
+        private Vector3 CalculateMaxBuoyancyForce()
+        {
+            float objectVolume = _rigidbody.mass / density;
+            Vector3 maxBuoyancyForce = BuoyancyObject._water.Density * objectVolume * -Physics.gravity;
+
+            return maxBuoyancyForce;
+        }
+
+        private Vector3[] Voxelization()
+        {
+            // record world data
+            Vector3 initialScale = transform.lossyScale;
+            Quaternion initialRotation = transform.rotation;
+            Vector3 initialPosition = transform.position;
+            Vector3 initialLocalScale = transform.localScale;
+
+            // back to local   
+            Vector3 parentScale = transform.parent.localScale;
+            transform.localScale = new Vector3(1f / parentScale.x, 1f / parentScale.y, 1f / parentScale.z);
+            transform.position = Vector3.zero;
+            transform.rotation = Quaternion.identity;
+
+            Physics.SyncTransforms();
+
+            Bounds bounds = _colliders[0].bounds;
+            _voxelSize.x = bounds.size.x * normalizedVoxelSize;
+            _voxelSize.y = bounds.size.y * normalizedVoxelSize;
+            _voxelSize.z = bounds.size.z * normalizedVoxelSize;
+            int voxelsCount = Mathf.RoundToInt(1f / normalizedVoxelSize);
+            List<Vector3> voxels = new List<Vector3>(voxelsCount * voxelsCount * voxelsCount);
+
+            for (int i = 0; i < voxelsCount; i++)
+            {
+                for (int j = 0; j < voxelsCount; j++)
+                {
+                    for (int k = 0; k < voxelsCount; k++)
+                    {
+                        float pX = bounds.min.x + _voxelSize.x * (0.5f + i);
+                        float pY = bounds.min.y + _voxelSize.y * (0.5f + j);
+                        float pZ = bounds.min.z + _voxelSize.z * (0.5f + k);
+                        Vector3 point = new Vector3(pX, pY, pZ);
+                        if (PointIsInsideCollider(_colliders[0], ref point))
+                        {
+                            voxels.Add(point);
+                        }
+                    }
+                }
+            }
+
+            // transform to world
+            transform.rotation = initialRotation;
+            transform.position = initialPosition;
+            transform.localScale = initialLocalScale;
+            _voxelSize.x *= initialScale.x;
+            _voxelSize.y *= initialScale.y;
+            _voxelSize.z *= initialScale.z;
+            return voxels.ToArray();
+        }
+
+        private bool PointIsInsideCollider(Collider c, ref Vector3 p)
+        {
+            var cp = Physics.ClosestPoint(p, c, Vector3.zero, Quaternion.identity);
+            return Vector3.Distance(cp, p) < 0.01f;
+        }
+
+        private void VoxelBounds(ref Bounds bounds)
+        {
+            bounds.SetMinMax(Vector3.zero, Vector3.zero);
+            foreach (var nextCollider in _colliders)
+            {
+                bounds.Encapsulate(nextCollider.bounds);
+            }
+        }
+
+
+        /// <summary>
+        /// 在救生圈刚接触水平面时，判断救生圈与溺水者的距离
+        /// 从而选择播放动画
+        /// </summary>
+        /// <param name="other"></param>
+
+        
+    }
+}
---
+++ Assets/QinYILearnProject/2.水的模拟和浮力的实现/Water/Scripts/Water/WaterWaves.cs
@@ -0,0 +1,380 @@
+﻿using UnityEngine;
+using Unity.Collections;
+using Unity.Jobs;
+//using Unity.Mathematics;
+
+namespace BuoyancyScope
+{
+    [RequireComponent(typeof(BoxCollider))]
+    [RequireComponent(typeof(MeshFilter))]
+    public class WaterWaves : MonoBehaviour
+    {
+        [Header("Water Wave")]
+        public bool useJobs = false;
+
+        [SerializeField]
+        private float speed = 1f;
+
+        [SerializeField]
+        private float height = 0.2f;
+
+        [SerializeField]
+        private float noiseWalk = 0.5f;
+
+        [SerializeField]
+        private float noiseStrength = 0.1f;
+
+        [Header("Water Volume")]
+        [SerializeField]
+        private float density = 1f;
+        public float Density { get { return density; } }
+        [SerializeField]
+        private int rows = 10;
+
+        [SerializeField]
+        private int columns = 10;
+
+        [SerializeField]
+        private float quadSegmentSize = 1f;
+
+        [Header("Water Debug")]
+        public bool debugMesh;
+        public bool debugNormal;
+        public bool debugTriangleSearch;
+        public Transform debugTransformForTrinagleSearch;
+
+        private Mesh mesh;
+        private Vector3[] baseVertices;
+        private Vector3[] vertices;
+        private Vector3[] worldVertices;
+        private NativeArray<Vector3> baseVerticesNative;
+        private NativeArray<Vector3> verticesNative;
+        private NativeArray<Vector3> worldVerticesNative;
+
+        protected void Awake()
+        {
+            ResizeBoxCollider();
+            mesh = GetComponent<MeshFilter>().mesh;
+            mesh.MarkDynamic();
+            if (useJobs)
+            {
+                baseVerticesNative = new NativeArray<Vector3>(mesh.vertices, Allocator.Persistent);
+                verticesNative = new NativeArray<Vector3>(mesh.vertexCount, Allocator.Persistent);
+                worldVerticesNative = new NativeArray<Vector3>(mesh.vertexCount, Allocator.Persistent);
+                for (int i = 0; i < mesh.vertexCount; i++)
+                {
+                    worldVerticesNative[i] = transform.TransformPoint(baseVerticesNative[i]);
+                }
+            }
+            else
+            {
+                baseVertices = mesh.vertices;
+                vertices = new Vector3[mesh.vertexCount];
+                worldVertices = new Vector3[mesh.vertexCount];
+                for (int i = 0; i < mesh.vertexCount; i++)
+                {
+                    worldVertices[i] = transform.TransformPoint(baseVertices[i]);
+                }
+            }
+        }
+
+        protected void Update()
+        {
+            if (useJobs)
+            {
+                WaterWavesJob wavesJob = new WaterWavesJob()
+                {
+                    vertices = verticesNative,
+                    baseVertices = baseVerticesNative,
+                    worldVertices = worldVerticesNative,
+                    time = Time.time,
+                    speed = speed,
+                    height = height,
+                    noiseWalk = noiseWalk,
+                    noiseStrength = noiseStrength,
+                    localScaleY = transform.localScale.y,
+                    worldMat = transform.localToWorldMatrix,
+                };
+                wavesJob.Schedule(verticesNative.Length, 64).Complete();
+                mesh.SetVertices(verticesNative);
+                mesh.RecalculateNormals();
+            }
+            else
+            {
+                for (var i = 0; i < vertices.Length; i++)
+                {
+                    var vertex = baseVertices[i];
+                    vertex.y +=
+                        Mathf.Sin(Time.time * speed + baseVertices[i].x + baseVertices[i].y + baseVertices[i].z) *
+                        (height / transform.localScale.y);
+
+                    vertex.y += Mathf.PerlinNoise(baseVertices[i].x + noiseWalk, baseVertices[i].y /*+ Mathf.Sin(Time.time * 0.1f)*/) * noiseStrength;
+
+                    vertices[i] = vertex;
+                    worldVertices[i] = transform.TransformPoint(vertex);
+                }
+                mesh.SetVertices(vertices);
+                mesh.RecalculateNormals();
+            }
+        }
+
+        protected void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.matrix = transform.localToWorldMatrix;
+            Gizmos.DrawWireCube(GetComponent<BoxCollider>().center, GetComponent<BoxCollider>().size);
+        }
+
+        protected void OnDrawGizmos()
+        {
+            if (!Application.isPlaying)
+            {
+                Gizmos.color = Color.cyan - new Color(0f, 0f, 0f, 0.75f);
+                Gizmos.matrix = transform.localToWorldMatrix;
+
+                Gizmos.DrawCube(GetComponent<BoxCollider>().center - Vector3.up * 0.01f, GetComponent<BoxCollider>().size);
+
+                Gizmos.color = Color.cyan - new Color(0f, 0f, 0f, 0.5f);
+                Gizmos.DrawWireCube(GetComponent<BoxCollider>().center, GetComponent<BoxCollider>().size);
+
+                Gizmos.matrix = Matrix4x4.identity;
+            }
+            else
+            {
+                // Debug sufrace normal
+                if (debugNormal)
+                {
+                    int[] triangles = mesh.triangles;
+                    if (useJobs)
+                    {
+                        NativeArray<Vector3> vertices = worldVerticesNative;
+                        for (int i = 0; i < triangles.Length; i += 3)
+                        {
+                            Gizmos.color = Color.white;
+                            Gizmos.DrawLine(vertices[triangles[i + 0]], vertices[triangles[i + 1]]);
+                            Gizmos.DrawLine(vertices[triangles[i + 1]], vertices[triangles[i + 2]]);
+                            Gizmos.DrawLine(vertices[triangles[i + 2]], vertices[triangles[i + 0]]);
+
+                            Vector3 center = MathfUtils.GetAveratePoint(vertices[triangles[i + 0]], vertices[triangles[i + 1]], vertices[triangles[i + 2]]);
+                            Vector3 normal = GetSurfaceNormal(center);
+
+                            Gizmos.color = Color.green;
+                            Gizmos.DrawLine(center, center + normal);
+                        }
+                    }
+                    else
+                    {
+                        var vertices = worldVertices;
+                        for (int i = 0; i < triangles.Length; i += 3)
+                        {
+                            Gizmos.color = Color.white;
+                            Gizmos.DrawLine(vertices[triangles[i + 0]], vertices[triangles[i + 1]]);
+                            Gizmos.DrawLine(vertices[triangles[i + 1]], vertices[triangles[i + 2]]);
+                            Gizmos.DrawLine(vertices[triangles[i + 2]], vertices[triangles[i + 0]]);
+
+                            Vector3 center = MathfUtils.GetAveratePoint(vertices[triangles[i + 0]], vertices[triangles[i + 1]], vertices[triangles[i + 2]]);
+                            Vector3 normal = GetSurfaceNormal(center);
+
+                            Gizmos.color = Color.green;
+                            Gizmos.DrawLine(center, center + normal);
+                        }
+                    }
+                }
+
+                // Debug mesh vertices
+                if (debugMesh)
+                {
+                    if (worldVertices != null)
+                    {
+                        for (int i = 0; i < worldVertices.Length; i++)
+                        {
+                            DebugUtils.DrawPoint(worldVertices[i], Color.red);
+                        }
+                    }
+                    else if (worldVerticesNative != null)
+                    {
+                        for (int i = 0; i < worldVerticesNative.Length; i++)
+                        {
+                            DebugUtils.DrawPoint(worldVerticesNative[i], Color.red);
+                        }
+                    }
+                }
+
+                // Test GetSurroundingTrianglePolygon
+                if (debugTriangleSearch)
+                {
+                    if (debugTransformForTrinagleSearch != null)
+                    {
+                        Gizmos.color = Color.blue;
+                        Gizmos.DrawSphere(debugTransformForTrinagleSearch.position, 0.1f);
+                        var point = debugTransformForTrinagleSearch.position;
+                        var triangle = GetSurroundingTrianglePolygon(point);
+                        if (triangle != null)
+                        {
+                            Gizmos.color = Color.red;
+                            Gizmos.DrawLine(triangle[0], triangle[1]);
+                            Gizmos.DrawLine(triangle[1], triangle[2]);
+                            Gizmos.DrawLine(triangle[2], triangle[0]);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if (useJobs)
+            {
+                if (verticesNative.IsCreated) verticesNative.Dispose();
+                if (baseVerticesNative.IsCreated) baseVerticesNative.Dispose();
+                if (worldVerticesNative.IsCreated) worldVerticesNative.Dispose();
+            }
+        }
+
+        private void ResizeBoxCollider()
+        {
+            var boxCollider = GetComponent<BoxCollider>();
+            if (boxCollider != null)
+            {
+                Vector3 center = boxCollider.center;
+                center.y = boxCollider.size.y / -2f;
+                center.y += (height + noiseStrength) / transform.localScale.y;
+
+                boxCollider.center = center;
+            }
+        }
+
+        struct WaterWavesJob : IJobParallelFor
+        {
+            [ReadOnly]
+            public NativeArray<Vector3> baseVertices;
+            public NativeArray<Vector3> vertices;
+            public NativeArray<Vector3> worldVertices;
+
+            [ReadOnly]
+            public float speed;
+
+            [ReadOnly]
+            public float noiseWalk;
+
+            [ReadOnly]
+            public float noiseStrength;
+
+            [ReadOnly]
+            public float time;
+
+            [ReadOnly]
+            public float height;
+
+            [ReadOnly]
+            public float localScaleY;
+
+            [ReadOnly]
+            public Matrix4x4 worldMat;
+
+            public void Execute(int index)
+            {
+                var vertex = baseVertices[index];
+                vertex.y += Mathf.Sin(time * speed + vertex.x + vertex.y + vertex.z) * (height / localScaleY);
+                // float noiseValue = Noise(vertex.x + noiseWalk, vertex.y);
+                vertex.y += Noise(vertex.x + noiseWalk, vertex.y /*+ Mathf.Sin(Time.time * 0.1f)*/) * noiseStrength;
+                vertices[index] = vertex;
+                var point = new Vector4(vertex.x, vertex.y, vertex.z, 1.0f);
+                worldVertices[index] = worldMat * point;
+            }
+
+            private float Noise(float x, float y)
+            {
+                //float2 pos = Unity.Mathematics.math.float2(x, y);
+                //return noise.snoise(pos);
+                return 0;
+            }
+        }
+
+        private int GetIndex(int row, int column)
+        {
+            return row * columns + row + column;
+        }
+
+        public Vector3 GetSurfaceNormal(Vector3 worldPoint)
+        {
+            Vector3[] meshPolygon = GetSurroundingTrianglePolygon(worldPoint);
+            if (meshPolygon != null)
+            {
+                Vector3 planeV1 = meshPolygon[1] - meshPolygon[0];
+                Vector3 planeV2 = meshPolygon[2] - meshPolygon[0];
+                Vector3 planeNormal = Vector3.Cross(planeV1, planeV2).normalized;
+                if (planeNormal.y < 0f)
+                {
+                    planeNormal *= -1f;
+                }
+
+                return planeNormal;
+            }
+
+            return transform.up;
+        }
+
+        public float GetWaterLevel(Vector3 worldP)
+        {
+            Vector3[] meshPolygon = GetSurroundingTrianglePolygon(worldP);
+            if (meshPolygon != null)
+            {
+                Vector3 p1 = meshPolygon[1] - meshPolygon[0];
+                Vector3 p2 = meshPolygon[2] - meshPolygon[0];
+                Vector3 pNorm = Vector3.Cross(p1, p2).normalized;
+                if (pNorm.y < 0f)
+                {
+                    pNorm *= -1f;
+                }
+                float yOnWaterSurface = (-(worldP.x * pNorm.x) - (worldP.z * pNorm.z) + Vector3.Dot(meshPolygon[0], pNorm)) / pNorm.y;
+                return yOnWaterSurface;
+            }
+
+            return transform.position.y;
+        }
+
+        public Vector3[] GetSurroundingTrianglePolygon(Vector3 worldPoint)
+        {
+            Vector3 localPoint = transform.InverseTransformPoint(worldPoint);
+            int x = Mathf.CeilToInt(localPoint.x / quadSegmentSize);
+            int z = Mathf.CeilToInt(localPoint.z / quadSegmentSize);
+            if (x <= 0 || z <= 0 || x >= (columns + 1) || z >= (rows + 1))
+            {
+                return null;
+            }
+
+            Vector3[] trianglePolygon = new Vector3[3];
+            if (useJobs)
+            {
+                if ((worldPoint - worldVerticesNative[GetIndex(z, x)]).sqrMagnitude <
+                    ((worldPoint - worldVerticesNative[GetIndex(z - 1, x - 1)]).sqrMagnitude))
+                {
+                    trianglePolygon[0] = worldVerticesNative[GetIndex(z, x)];
+                }
+                else
+                {
+                    trianglePolygon[0] = worldVerticesNative[GetIndex(z - 1, x - 1)];
+                }
+                trianglePolygon[1] = worldVerticesNative[GetIndex(z - 1, x)];
+                trianglePolygon[2] = worldVerticesNative[GetIndex(z, x - 1)];
+            }
+            else
+            {
+                if ((worldPoint - worldVertices[GetIndex(z, x)]).sqrMagnitude <
+                    ((worldPoint - worldVertices[GetIndex(z - 1, x - 1)]).sqrMagnitude))
+                {
+                    trianglePolygon[0] = worldVertices[GetIndex(z, x)];
+                }
+                else
+                {
+                    trianglePolygon[0] = worldVertices[GetIndex(z - 1, x - 1)];
+                }
+                trianglePolygon[1] = worldVertices[GetIndex(z - 1, x)];
+                trianglePolygon[2] = worldVertices[GetIndex(z, x - 1)];
+            }
+
+            return trianglePolygon;
+        }
+    }
+}
